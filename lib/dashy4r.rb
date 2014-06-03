e88d1a6be11d485c20ac01124bab181e5b96e69a
Alabê Duarte <aduarte@thoughtworks.com>
2014-06-03 16:52:09 -0300

    +blins - Removed useless comments

--- lib/dashy4r.rb
+++ lib/dashy4r.rb
@@ -1,8 +1,3 @@
-# require 'dashy/base'
-# require 'dashy/config_yml'
-# require 'dashy/rest_interface'
-# require 'dashy/runner'
-
 require "#{File.dirname(__FILE__)}/dashy/base"
 require "#{File.dirname(__FILE__)}/dashy/config_yml"
 require "#{File.dirname(__FILE__)}/dashy/rest_interface"
