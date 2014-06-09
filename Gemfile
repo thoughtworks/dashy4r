46a574e0476731b98edc0f1b816724aee24a7565
Alabê Duarte <aduarte@thoughtworks.com>
2014-06-09 18:15:02 -0300

    Added unit tests to cover Dashy::RestInterface

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
 source "http://rubygems.org"
 
+gem 'system_timer', '~> 1.2.4'
+gem 'mime-types', '~> 1.16'
+gem 'rest-client', '~> 1.6.7'
+gem 'nokogiri', '~> 1.5.10'
+gem "webmock"
+
 group :development do
-  gem 'system_timer', '~> 1.2.4'
-  gem 'mime-types', '~> 1.16'
-  gem 'rest-client', '~> 1.6.7'
-  gem 'nokogiri', '~> 1.5.10'
   gem "bundler", "~> 1.0"
   gem "jeweler", "~> 2.0.1"
 end
--- spec/dashy/rest_interface_spec.rb
+++ spec/dashy/rest_interface_spec.rb
@@ -1,9 +1,49 @@
 require 'spec_helper'
 require './lib/dashy4r'
+require 'webmock/rspec'
 
-describe Dashy::Base do
-  xit "should report status" do
-    success_report = ({:success => true }).merge(options)
+describe Dashy::RestInterface do
+  let(:options) {{ :environment => 'test', :endpoint => 'a_service' }}
+  let(:success_report) { ({:success => true }).merge(options) }
+  let(:failure_report) { ({:success => false }).merge(options) }
+  let(:dashy_local_url) do
+    config = Dashy::Base.config
+    "#{config.url}/requests/#{config.app_id}"
+  end
+
+  before(:all) do
+    file_path = "#{File.dirname(__FILE__)}/../dashy_config.yml"
+    Dashy::Base.enable_with_config file_path
+  end
+
+  it "should report status with success" do
+    stub_report_request! :success => true
+
     Dashy::RestInterface.report_status success_report
+
+    expect(WebMock).to have_requested(:post, dashy_local_url).with success_report
+  end
+
+  it "should report status with failure" do
+    stub_report_request! :success => false
+
+    Dashy::RestInterface.report_status failure_report
+
+    expect(WebMock).to have_requested(:post, dashy_local_url).with success_report
+  end
+
+  def stub_report_request!(status)
+    stub_request(:post, dashy_local_url).
+      with(
+        :body =>
+          {"request"=>
+            {
+              "environment"=> options[:environment].to_s,
+              "endpoint"=> options[:endpoint].to_s,
+              "success"=> status[:success].to_s
+            }
+          },
+        :headers => {'Content-Type'=>'application/x-www-form-urlencoded'}
+      ).to_return(:status => 200, :body => "", :headers => {})
   end
 end
